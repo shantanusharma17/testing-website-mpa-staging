c13e2edffad2b1b75049f2855d98e4005c638261
Shantanu Sharma <shantanu.sharma@compass.com>
2024-08-01 14:29:58 -0400

    fix path

--- src/components/NavigationHeader/NavigationHeader.jsx
+++ src/components/NavigationHeader/NavigationHeader.jsx
@@ -5,24 +5,24 @@ const NavigationHeader = () => {
   return (
     <header className="header">
       <nav className="navbar">
-        <a href="/testing-website-mpa#/" className="logo">
+        <a href="/testing-website-mpa-staging#/" className="logo">
           Home
         </a>
         <div className="nav-links">
-          <a href="/testing-website-mpa#/contact" className="nav-link">
+          <a href="/testing-website-mpa-staging#/contact" className="nav-link">
             Contact
           </a>
-          <a href="/testing-website-mpa#/properties" className="nav-link">
+          <a href="/testing-website-mpa-staging#/properties" className="nav-link">
             Properties
           </a>
-          <a href="/testing-website-mpa#/listings" className="nav-link">
+          <a href="/testing-website-mpa-staging#/listings" className="nav-link">
             Listings
           </a>
-          {/*<a href="/testing-website-mpa#/properties-address-mls" className="nav-link">*/}
+          {/*<a href="/testing-website-mpa-staging#/properties-address-mls" className="nav-link">*/}
           {/*  Properties By Address + MLS*/}
           {/*</a>*/}
         </div>
-        <a href="/testing-website-mpa#/login" className="nav-link">
+        <a href="/testing-website-mpa-staging#/login" className="nav-link">
           Login
         </a>
       </nav>
